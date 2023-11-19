993c88b4cd859d04ac6a4fe33f8204d1fd36315e
summermong <2514o_o@naver.com>
2023-11-20 03:33:00 +0900

    FEAT: 찜 기능 구현

--- src/tap/fav/FavTap.js
+++ src/tap/fav/FavTap.js
@@ -1,61 +1,41 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Styles from './FavTap.module.css';
+import axios from 'axios';
 import RoundTap from '../../common/btn/RoundTap';
 import Posting from '../../common/userposting/Posting';
 
 const FavTap = () => {
   const [isMakerActive, setIsActiveMaker] = useState(true);
+  const [userMarkerArr, setUserMarkerArr] = useState([]);
 
   const handleActiveRoute = () => {
     setIsActiveMaker(!isMakerActive);
   };
 
-  const userMarkerArr = [
-    {
-      title: '홍대 개미',
-      writer: '작성자',
-      date: '2023-10-1',
-      fav: true,
-      latitude: 0,
-      longitued: 1,
-    },
-    { title: '피오니', writer: '작성자', date: '2023-10-2', fav: true, latitude: 0, longitued: 1 },
-    { title: '탕후루', writer: '작성자', date: '2023-10-3', fav: true, latitude: 0, longitued: 1 },
-    { title: '현우동', writer: '작성자', date: '2023-10-4', fav: true, latitude: 0, longitued: 1 },
-    {
-      title: '슈붕 파는 곳',
-      writer: '작성자',
-      date: '2023-10-5',
-      fav: true,
-      latitude: 0,
-      longitued: 1,
-    },
-    {
-      title: '파주 영어마을',
-      writer: '작성자',
-      date: '2023-10-5',
-      fav: true,
-      latitude: 0,
-      longitued: 1,
-    },
-  ];
+  useEffect(() => {
+    axios
+      .get('https://localhost:8080/star/markers', {
+        headers: {
+          Authorization: 'jwt',
+        },
+      })
+      .then(response => {
+        setUserMarkerArr(response.data);
+      })
+      .catch(console.log('아직 없음'));
+  }, []);
 
   const userRouteArr = [
     {
-      title: '파주 여행',
-      writer: '작성자',
-      date: '2023-10-1',
-      fav: true,
-      latitude: 0,
-      longitued: 1,
-    },
-    {
-      title: '수원 데이트',
-      writer: '작성자',
-      date: '2023-10-2',
-      fav: true,
-      latitude: 0,
-      longitued: 1,
+      id: 2,
+      nickName: '황윤',
+      title: '수원역',
+      memo: '수원역에 왔다!',
+      location: {
+        latitude: 127.001443714087,
+        longitude: 37.300455081,
+      },
+      wentDate: '2023-11-02T15:30',
     },
   ];
 
@@ -68,9 +48,8 @@ const FavTap = () => {
             <Posting
               key={index}
               title={marker.title}
-              writer={marker.writer}
-              date={marker.date}
-              fav={marker.fav}
+              nickName={marker.nickName}
+              wentDate={marker.wentDate.slice(0, 10)}
             />
           ))}
         {!isMakerActive &&
@@ -78,9 +57,8 @@ const FavTap = () => {
             <Posting
               key={index}
               title={route.title}
-              writer={route.writer}
-              date={route.date}
-              fav={route.fav}
+              nickName={route.nickName}
+              wentDate={route.wentDate.slice(0, 10)}
             />
           ))}
       </div>
