cb93265e415b4d9eb4cdb3653ba8708003312367
강경규 <qwzx16@naver.com>
2023-11-26 21:31:45 +0900

    CHORE: 카카오 공유하기를 위한 셋팅

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ if (process.env.NODE_ENV === 'development') {
     onUnhandledRequest: 'bypass', // 미처리된 요청에 대한 경고를 무시하고 진행합니다.
   });
 }
-
+window.Kakao.init(process.env.REACT_APP_KAKAO_API_KEY);
+window.Kakao.isInitialized();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <RecoilRoot>
