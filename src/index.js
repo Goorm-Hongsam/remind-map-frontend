5b4a455aa4e274df69c0c0b249715817171bc83a
강경규 <qwzx16@naver.com>
2023-11-26 22:04:34 +0900

    CHORE : msw 제거

--- src/index.js
+++ src/index.js
@@ -2,13 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { RecoilRoot } from 'recoil';
-import { worker } from './mooks/worker';
 
-if (process.env.NODE_ENV === 'development') {
-  worker.start({
-    onUnhandledRequest: 'bypass', // 미처리된 요청에 대한 경고를 무시하고 진행합니다.
-  });
-}
 window.Kakao.init(process.env.REACT_APP_KAKAO_API_KEY);
 window.Kakao.isInitialized();
 const root = ReactDOM.createRoot(document.getElementById('root'));
