e2c0ab6041350f81f5333ee866ef1f2672ce531a
강경규 <qwzx16@naver.com>
2023-11-27 16:39:51 +0900

    STYLE: CSS 수정 이미지 변수 추가

--- src/tap/group/components/atom-components/GroupUserCard.js
+++ src/tap/group/components/atom-components/GroupUserCard.js
@@ -7,21 +7,21 @@ export default function GroupUserCard({ member, userName, buttonText, onClick, b
       onClick={onClick}
       className="w-full border-b border-main-color flex items-center justify-center relative gap-3 p-1"
     >
-      {member.profile_thumbnail_image ? (
+      {member.profile_thumbnail_image || member.thumbnailImageUrl ? (
         <img
           className="w-8 rounded-full"
           src={member.profile_thumbnail_image || member.thumbnailImageUrl}
-          alt=""
+          alt="유저 프로필 이미지"
         />
       ) : (
         <img
           className="w-8 rounded-full"
           src="https://img.icons8.com/?size=128&id=ckaioC1qqwCu&format=png"
-          alt=""
+          alt="기본 프로필 이미지"
         />
       )}
 
-      <p class="text-sm pb-1 whitespace-nowrap overflow-x-auto w-24">{userName}</p>
+      <p class="text-xs pb-1 whitespace-nowrap overflow-x-auto w-24">{userName}</p>
       <div className="grow" />
       {buttonText && (
         <GroupButton
